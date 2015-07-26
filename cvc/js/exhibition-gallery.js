36ef2e93b3f0717475074b902be902ef6c201af1
PradatiusD <danielprada2012@gmail.com>
2015-07-25 20:52:07 -0400

    Added ability to click between images and the ability use navigational arrows to move through images

--- cvc/js/exhibition-gallery.js
+++ cvc/js/exhibition-gallery.js
@@ -5,26 +5,47 @@
 
   function cvcGallery ($gallery) {
 
-    $gallery.prepend('<article class="featured row"><figure></figure><figcaption class="small-12 columns text-center"></figcaption></article>');
+    var featuredHTML = '<article class="featured row">' +
+                         '<figure></figure>' +
+                         '<a href="javascript:void(0);" class="fa fa-angle-left"></a>' +
+                         '<a href="javascript:void(0);" class="fa fa-angle-right"></a>' +
+                         '<figcaption class="small-12 columns text-center"></figcaption>' +
+                       '</article>';
 
-    this.$figure       = $gallery.find('article').find('figure');
-    this.$descriptions = $gallery.find('article').find('figcaption');
+    // Featured HTML
+    $gallery.prepend(featuredHTML);
+
+    // Make associations
+    this.$featured     = $gallery.find('article');
+    this.$figure       = this.$featured.find('figure');
+    this.$descriptions = this.$featured.find('figcaption');
+
+    this.$arrows       = this.$featured.find('.fa');
+    this.$leftArrow    = this.$featured.find('.fa-angle-left');
+    this.$rightArrow   = this.$featured.find('.fa-angle-right');
+    
     this.$gallery      = $gallery;
+
+    // Init with image index
+    this.imgIndex = 0;
+
 
     return this;
   }
 
-  cvcGallery.prototype.createGallery = function () {
+  cvcGallery.prototype.setImageClickHandlers = function () {
 
+    var $parent       = this;
     var $gallery      = this.$gallery;
-
     var $figure       = this.$figure;
     var $descriptions = this.$descriptions;
     var $featuredSec  = $figure.add($descriptions);
+    var imageIndex    = this.imgIndex;
+    var $images       = $gallery.find('img');
 
     $featuredSec.addClass('animated');
 
-    $gallery.find('img').each(function (imgIndex) {
+    $images.each(function (imgIndex) {
 
       var $this  = $(this);
       var imgSrc = $this.attr('src');
@@ -32,6 +53,9 @@
       var id     = data.attachmentId;
 
       $this.click(function (e) {
+
+        $images.removeClass('active');
+        $this.addClass('active');
 
         $featuredSec.addClass('fade-in');
         setTimeout(function (){
@@ -70,20 +94,58 @@
         // If no data add button to edit it
         if (Object.keys(meta).length === 0 && isLoggedIn) {
           descriptions += '<h6 class="subheader">No Metadata found</p>';
-          descriptions += "<a class=\"secondary button\" href=\"http://localhost/cvc/wp-admin/post.php?post="+id+"&action=edit\" target=\"blank\">Edit Metadata</a>";
+          // http://localhost/cvc/wp-admin/pos
+          descriptions += "<a class=\"secondary button\" href=\"http://cvc.pradadesigners.com/wp-admin/post.php?post="+id+"&action=edit\" target=\"blank\">Edit Metadata</a>";
         }
 
         $figure.html('<img src="'+imgSrc+'"/>');
 
         $descriptions.html(descriptions);
 
+        var newImgHeight = $figure.find('img').height() + 'px';
+
+        $parent.imgIndex = imgIndex;
+        $parent.$arrows.css({'line-height':newImgHeight});
 
       });
 
-      if (imgIndex === 0) {
+      if (imgIndex === imageIndex) {
         $this.trigger('click');
       }
     });
+  };
+
+
+  cvcGallery.prototype.setArrowClickHandlers = function () {
+
+    var $this = this;
+
+    function returnArrowClickHandler (direction) {
+
+      return function (e) {
+        e.preventDefault();
+
+        if (direction == 'right') {
+          $this.imgIndex++;
+        } else if (direction == 'left') {
+          $this.imgIndex--;
+        }
+
+        var $images   = $this.$gallery.find('.images').find('img');
+        var targetImg = $images.eq($this.imgIndex);
+
+        $images.eq($this.imgIndex).trigger('click');
+
+      };
+    }
+
+    this.$leftArrow.click(returnArrowClickHandler('left'));
+    this.$rightArrow.click(returnArrowClickHandler('right'));
+  };
+
+  cvcGallery.prototype.createGallery = function () {
+    this.setImageClickHandlers();
+    this.setArrowClickHandlers();
   };
 
 
