576dbb37f3d7c8ecc2f329ea7c6eaba939c1e237
PradatiusD <danielprada2012@gmail.com>
2016-07-04 15:33:38 -0400

    Updated export script

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,85 +2,94 @@ require('dotenv').config();
 
 module.exports = function(grunt) {
 
-  grunt.initConfig({
-    php: {
-      test: {
-        options: {
-          keepalive: true,
-          open: true,
-          port: 5000
-        }
-      }
-    },
-    'ftp-deploy': {
-      main: {
-        auth: {
-          host: process.env.PRIMARY_FTP_HOST,
-          port: 21,
-          username: process.env.PRIMARY_FTP_USERNAME,
-          password: process.env.PRIMARY_FTP_PASSWORD
-        },
-        src: 'cvc',
-        dest: 'wp-content/themes/cvc',
-        exclusions: [
-	        'cvc/img/*',
-	        '.DS_Store',
-	        'favicon.ico',
-	        'cvc/screenshot.png'
-        ],
-        forceVerbose: true
-      }
-    },
-    copy: {
-      main: {
-        files: [
-          {
-            expand: true,
-            src: ['cvc/**'],
-            dest: process.env.LOCAL_THEME_DIRECTORY
-          }
-        ]
-      }
-    },
-    sass: {
-      dist: {
-        options: {
-          style: 'expanded'
-        },
-        files: {
-          'cvc/style.css': 'style.sass'
-        }
-      }
-    },
-    watch: {
+  var options = {};
+
+  options.php = {
+    test: {
       options: {
-        livereload: true
-      },
-      theme: {
-        files: ['cvc/*.php', 'cvc/lib/*.php', 'cvc/js/*.js'],
-        tasks: ['copy']
-      },
-      sassy: {
-        files: 'style.sass',
-        tasks: ['sass', 'copy']
-      },
-      homepageJS: {
-        files: 'cvc/js/homepage/*.js',
-        tasks: ['uglify', 'copy']
-      }
-    },
-    uglify: {
-      global: {
-        files: {
-          'cvc/js/global.min.js': [
-	          'bower_components/foundation/js/foundation.js',
-	          'bower_components/foundation/js/foundation/foundation.topbar.js',
-	          'bower_components/imagelightbox/dist/imagelightbox.min.js'
-          ]
-        }
+        keepalive: true,
+        open: true,
+        port: 5000
       }
     }
-  });
+  }
+
+  options['ftp-deploy'] = {
+    main: {
+      auth: {
+        host: process.env.PRIMARY_FTP_HOST,
+        port: 21,
+        username: process.env.PRIMARY_FTP_USERNAME,
+        password: process.env.PRIMARY_FTP_PASSWORD
+      },
+      src: 'cvc',
+      dest: 'wp-content/themes/cvc',
+      exclusions: [
+        'cvc/img/*',
+        '.DS_Store',
+        'favicon.ico',
+        'cvc/screenshot.png'
+      ],
+      forceVerbose: true
+    }
+  }
+
+  options.copy = {
+    main: {
+      files: [
+        {
+          expand: true,
+          src: ['cvc/**'],
+          dest: process.env.LOCAL_THEME_DIRECTORY
+        }
+      ]
+    }
+  };
+
+  options.sass = {
+    dist: {
+      options: {
+        style: 'expanded'
+      },
+      files: {
+        'cvc/style.css': 'style.sass'
+      }
+    }
+  }
+
+
+  options.watch = {
+    options: {
+      livereload: true
+    },
+    theme: {
+      files: ['cvc/*.php', 'cvc/lib/*.php', 'cvc/js/*.js'],
+      tasks: ['copy']
+    },
+    sassy: {
+      files: 'style.sass',
+      tasks: ['sass', 'copy']
+    },
+    homepageJS: {
+      files: 'cvc/js/homepage/*.js',
+      tasks: ['uglify', 'copy']
+    }
+  }
+
+  options.uglify = {
+    global: {
+      files: {
+        'cvc/js/global.min.js': [
+          'bower_components/foundation/js/foundation.js',
+          'bower_components/foundation/js/foundation/foundation.topbar.js',
+          'bower_components/imagelightbox/dist/imagelightbox.min.js'
+        ]
+      }
+    }
+  }
+
+  grunt.initConfig(options);
+
   grunt.loadNpmTasks('grunt-php');
   grunt.loadNpmTasks('grunt-ftp-deploy');
   grunt.loadNpmTasks('grunt-concurrent');
@@ -88,6 +97,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+
   grunt.registerTask('default', ['watch']);
   grunt.registerTask('deploy', ['ftp-deploy']);
   grunt.registerTask('js', ['uglify', 'copy']);
---
+++ cvc/gallery-export.php
@@ -0,0 +1,73 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require_once('./../../../wp-load.php');
+
+function get_data ($sql) {
+
+  global $wpdb;
+  $results = $wpdb->get_results($sql);
+  return $results;
+}
+
+function maybeEncodeCSVField($string) {
+  if(strpos($string, ',') !== false || strpos($string, '"') !== false || strpos($string, "\n") !== false) {
+      $string = '"' . str_replace('"', '""', $string) . '"';
+  }
+  return $string;
+}
+
+
+$post_meta   = get_data('SELECT * FROM wp_postmeta WHERE meta_key LIKE "wpcf-%"');
+$attachments = get_data('SELECT ID, guid, post_title FROM wp_posts WHERE post_type="attachment" and post_mime_type LIKE "image%"');
+
+
+for ($i=0; $i < count($attachments); $i++) {
+
+  $attachment_post_id = $attachments[$i]->ID;
+
+  for ($j=0; $j < count($post_meta); $j++) {
+
+    $post_meta_post_id = $post_meta[$j]->post_id;
+
+    if ($post_meta_post_id === $attachment_post_id) {
+
+      $meta_key = $post_meta[$j]->meta_key;
+      $meta_value = $post_meta[$j]->meta_value;
+
+      if ($meta_key && $meta_value) {
+        $attachments[$i]->$meta_key = $meta_value;
+      }
+    }
+  }
+}
+
+
+// output headers so that the file is downloaded rather than displayed
+header('Content-type: text/csv');
+header('Content-Disposition: attachment; filename="Gallery Export '.date("F j, Y").'.csv"');
+ 
+// do not cache the file
+header('Pragma: no-cache');
+header('Expires: 0');
+ 
+// create a file pointer connected to the output stream
+$file = fopen('php://output', 'w');
+ 
+// send the column headers
+fputcsv($file, array('Post ID', 'Image URL', 'Image Title', 'Artist Name', 'Work Name', 'Year Made', 'Work Medium', 'Year'));
+ 
+// Sample data. This can be fetched from mysql too
+ 
+// output each row of the data
+foreach ($attachments as $row){
+  fputcsv($file, (array) $row);
+}
+ 
+exit();
+
+
+?>
